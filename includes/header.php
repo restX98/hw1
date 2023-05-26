87c508e85e845509c98330b01352c82d280862ae
Enrico Restuccia <enrico.rest98@gmail.com>
2023-05-27 01:45:54 +0200

    Add cart and user icons

--- includes/header.php
+++ includes/header.php
@@ -7,19 +7,23 @@
 <header>
     <div class="top-bar">
         <div class="container">
-        <div class="logo">
-            <a href="#">Logo</a>
-        </div>
-        <div class="search-bar">
-            <input type="text" placeholder="Search">
-            <button type="submit">Search</button>
-        </div>
-        <div class="cart">
-            <a href="#">Cart</a>
-        </div>
-        <div class="user-banner">
-            <a href="#">User Banner</a>
-        </div>
+            <div class="logo">
+                <a href="#">Logo</a>
+            </div>
+            <div class="search-bar">
+                <input type="text" placeholder="Search">
+                <button type="submit">Search</button>
+            </div>
+            <div class="user">
+                <a href="/hw1/login">
+                    <img src="/hw1/client/icons/user.svg" alt="User Icon">
+                </a>
+            </div>
+            <div class="cart">
+                <a href="#">
+                    <img src="/hw1/client/icons/cart.svg" alt="Cart Icon">
+                </a>
+            </div>
         </div>
     </div>
     <div class="bottom-bar">
